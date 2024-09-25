07b6a4368b3248134e254ca99669376c91383fc7
Ramansinghparihar <parihar.ramansingh11@gmail.com>
2024-09-25 06:54:29 +0530

    Add files via upload

---
+++ a is big thnb.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int a,b;
+  cout<<"enter value of a"<<endl;
+  cin>>a;
+  cout<<"enter value of b"<<endl;
+  cin>>b;
+  if(a>b){
+    cout<<"A is big"<<endl;
+    
+  }
+  else{
+    cout<<"b is big"<<endl;
+  }
+   return 0;
+
+}
