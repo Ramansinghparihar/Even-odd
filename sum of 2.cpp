99caa8099aa73770a7860529e721ba579fc096bd
Ramansinghparihar <parihar.ramansingh11@gmail.com>
2024-09-25 07:00:25 +0530

    Add files via upload

---
+++ sum of 2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include<math.h>
+using namespace std;
+int main() {
+ int a,b;
+  cout<<"value of a"<<endl;
+  cin>>a;
+  cout<<"value of b"<<endl;
+  cin>>b;
+  int sum=a+b;
+  cout<<sum<<endl;
+  
+    }
